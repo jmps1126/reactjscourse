05f25534dfaad45a3069876b1581a8f81757fcb8
juan.pulgarin <juan.pulgarin@quipux.com>
2019-03-03 13:22:38 -0500

    se realiza primer commit del curso de reactjs

---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Location from './Location'
+import WeatherData from './WeatherData'
+
+const WeatherLocation = () => (
+    <div>
+        <Location city ={'Colombia'}/>
+        <hr/>
+        <WeatherData/>
+    </div>
+);
+
+export default WeatherLocation;
