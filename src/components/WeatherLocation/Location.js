6e820f2bbb21566d1ae21bbea0f7e829a519b7f2
juan.pulgarin <juan.pulgarin@quipux.com>
2019-03-03 15:24:34 -0500

    se realia commit del refactor del proyecto

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const Location = ({city}) => (
     <div>
         <h1>{city}</h1>
     </div>
 );
+
+Location.prototype = {
+    city: PropTypes.string.isRequired,
+};
 
 export default Location;
--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import {
+    SUN,
+} from '../../../constants/weathers';
 
 const WeatherData = () => (
     <div>
-        <WeatherTemperature temperature = {20} weatherState = {''}/>
+        <WeatherTemperature temperature={20} weatherState={SUN}/>
         <WeatherExtraInfo humidity={80} wind={"20 mts/s"}/>
     </div>
 );
