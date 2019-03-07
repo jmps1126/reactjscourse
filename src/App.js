bc920230aac20a4d2c75f126ce2c7516c187da83
juan.pulgarin <juan.pulgarin@quipux.com>
2019-03-07 10:52:29 -0500

    se realizan avances

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import WeatherLocation from './components/WeatherLocation';
-import './App.css';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Weather App (aplicación del clima)!
           <WeatherLocation></WeatherLocation>
         </header>
       </div>
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import convert from 'convert-units';
 import Location from './Location'
 import WeatherData from './WeatherData'
 import './styles.css';
 import {
-    SUN,
-    RAIN
+    SUN
 } from './../../constants/weathers'
+
+const Location_country = "London,uk";
+const api_key = "361c49a12ced117e86fc92b052d60651";
+const url_base = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base}?q=${Location_country}&appid=${api_key}`;
 
 const data  = {
     temperature: 5,
@@ -14,12 +20,6 @@ const data  = {
     wind: '10 m/s'
 };
 
-const data2 = {
-    temperature: 15,
-    weatherState: RAIN,
-    humidity: 30,
-    wind: '20 m/s'
-};
 
 
 class WeatherLocation extends Component {
@@ -31,11 +31,45 @@ class WeatherLocation extends Component {
             data: data
         }
     }
+
+    getTemp = kelvin =>{
+        return Number(convert(kelvin).from("K").to("C").toFixed(2));
+    };
+
+    getWeatherState = weather_data => {
+        return SUN;
+    };
+
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+        const temperature = this.getTemp(temp);
+       
+        const data = {
+            humidity,
+            temperature,
+            weatherState,
+            wind: `${speed} m/s`
+        }
+
+        return data;
+    }
+
     
     handleUpdateClick = () =>{
-        this.setState({
-            city: 'Madrid',
-            data: data2
+        fetch(api_weather).then( resolve => {
+            return resolve.json();
+        }).then(data => {
+            const newData = this.getData(data);
+            const newCity = data.name;
+
+            console.log(newData);
+            this.setState({
+                data: newData,
+                city: newCity,
+            });
+
         });
     };
 
@@ -45,7 +79,7 @@ class WeatherLocation extends Component {
             <div className="weatherLocationCont">
                 <Location city ={city}/>
                 <WeatherData data={data}/>
-                <button onClick={this.handleUpdateClick} >Actualizar</button>
+                <button id="buttonUpdate" onClick={this.handleUpdateClick} >Actualizar</button>
             </div>
         );
     }
